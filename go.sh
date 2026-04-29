3b1541f7fd758ff6d1ec5686f2d036a4dacb62bd
workspaceuser <workspaceuser@plot.ly>
2026-04-29 18:46:20 +0000

    reduce to a redirect app to put traffic onto the new /usv endpoint

--- go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# launch workes locally for background task testing.
-celery -A app:celery_app worker --loglevel=DEBUG --concurrency=12 --without-gossip &
-celery -A app:celery_app beat &
-/app/.heroku/python/bin/python /workspace/app.py
-
-
--- shutdown.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pkill -f "python.*9018"
